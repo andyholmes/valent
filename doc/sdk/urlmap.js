28aeae67546d77641964b7fdc1ca24351b84ab0c
Andy Holmes <andrew.g.r.holmes@gmail.com>
2021-06-27 14:16:57 -0700

    Documentation: improvements
    * fix logo
    * fix EDS dependency and remove redundancy
    * add libadwaita to urlmap.js

--- doc/sdk/urlmap.js
+++ doc/sdk/urlmap.js
@@ -6,5 +6,6 @@ baseURLs = [
     [ 'Gdk', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4/' ],
     [ 'Gsk', 'https://gnome.pages.gitlab.gnome.org/gtk/gsk4/' ],
     [ 'Gtk', 'https://gnome.pages.gitlab.gnome.org/gtk/gtk4/' ],
+    [ 'Adw', 'https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/' ],
 ]
 
