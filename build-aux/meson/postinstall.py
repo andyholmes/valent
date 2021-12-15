8244d2b4798b7e7dbacf30a79b9d51c993f455c9
Andy Holmes <andrew.g.r.holmes@gmail.com>
2021-12-15 14:25:07 -0800

    meson: misc tweaks
    * add common linker args
    * add "pic" to static libraries
    * use a cleaner summary() format
    * add the post-install script from the root meson.build

--- build-aux/meson/postinstall.py
+++ build-aux/meson/postinstall.py
@@ -2,23 +2,24 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2021 Andy Holmes <andrew.g.r.holmes@gmail.com>
 
-
 # pylint: disable=missing-module-docstring
 
-from os import environ, path
-from subprocess import call
+import os
+import subprocess
 
-prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-datadir = path.join(prefix, 'share')
-destdir = environ.get('DESTDIR', '')
+prefix = os.environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = os.path.join(prefix, 'share')
 
-# Package managers set this so we don't need to run
-if not destdir:
+# If DESTDIR is defined, a package manager will be handling this
+if 'DESTDIR' not in os.environ:
+    print('Compiling GSettings schemas...')
+    subprocess.call(['glib-compile-schemas',
+                     os.path.join(datadir, 'glib-2.0', 'schemas')])
+
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+    subprocess.call(['gtk-update-icon-cache', '-qtf',
+                     os.path.join(datadir, 'icons', 'hicolor')])
 
     print('Updating desktop database...')
-    call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
-
-    print('Compiling GSettings schemas...')
-    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
+    subprocess.call(['update-desktop-database', '-q',
+                     os.path.join(datadir, 'applications')])
