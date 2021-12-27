85949913ab32699190dc98622b77b3186a9d5a1d
Andy Holmes <andrew.g.r.holmes@gmail.com>
2021-12-27 06:18:39 -0800

    meson: more fixes and cleanups
    * bump meson dependency to 0.56 and fix deprecations
    * fix build on clang, passing `-Wno-error=unused-but-set-variable` to
      avoid false positives with cleanup functions
    * remove unused project arguments `-DVALENT_COMPILATION` and
      `-DHAVE_CONFIG_H`
    * remove application name suffix for devel builds
    * fix bogus positional argument on i18n.merge_file()
      see: https://github.com/mesonbuild/meson/issues/9441
    * misc style cleanups

--- src/main.c
+++ src/main.c
@@ -14,19 +14,16 @@ main (int   argc,
 {
   int ret;
   g_autoptr (ValentApplication) service = NULL;
-  g_autofree char *application_name = NULL;
 
   /* Set up gettext translations */
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
 
-  application_name = g_strconcat ("Valent", PROFILE_SUFFIX, NULL);
-  g_set_application_name (application_name);
-
   /* Start the service */
   valent_debug_init ();
 
+  g_set_application_name ("Valent");
   service = _valent_application_new ();
   ret = g_application_run (G_APPLICATION (service), argc, argv);
 
