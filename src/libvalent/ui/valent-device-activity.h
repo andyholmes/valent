9deff040cb1acd08d8777655a2b592ca79a0e3ff
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-03-06 00:57:02 -0800

    ValentDeviceActivity, ValentDeviceGadget: minor cleanups
    * require GtkWidget
    * cleanup documentation

--- src/libvalent/ui/valent-device-activity.h
+++ src/libvalent/ui/valent-device-activity.h
@@ -7,6 +7,7 @@
 # error "Only <libvalent-ui.h> can be included directly."
 #endif
 
+#include <gtk/gtk.h>
 #include <libvalent-core.h>
 
 G_BEGIN_DECLS
@@ -14,7 +15,7 @@ G_BEGIN_DECLS
 #define VALENT_TYPE_DEVICE_ACTIVITY (valent_device_activity_get_type ())
 
 VALENT_AVAILABLE_IN_1_0
-G_DECLARE_INTERFACE (ValentDeviceActivity, valent_device_activity, VALENT, DEVICE_ACTIVITY, GObject)
+G_DECLARE_INTERFACE (ValentDeviceActivity, valent_device_activity, VALENT, DEVICE_ACTIVITY, GtkWidget)
 
 struct _ValentDeviceActivityInterface
 {
