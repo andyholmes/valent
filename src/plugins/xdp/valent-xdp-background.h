76e804c51566226bbf4683345bc57e36601c6cf2
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-03-25 20:19:56 -0700

    plugins: add ValentXdpBackground
    Add a plugin for requesting background operation and autostart
    functionality.

---
+++ src/plugins/xdp/valent-xdp-background.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2022 Andy Holmes <andrew.g.r.holmes@gmail.com>
+
+#pragma once
+
+#include <gio/gio.h>
+#include <libvalent-core.h>
+
+G_BEGIN_DECLS
+
+#define VALENT_TYPE_XDP_BACKGROUND (valent_xdp_background_get_type())
+
+G_DECLARE_FINAL_TYPE (ValentXdpBackground, valent_xdp_background, VALENT, XDP_BACKGROUND, ValentApplicationPlugin)
+
+G_END_DECLS
+
