5f8560e050fce476d0bbe989531a42f0e2f6a73a
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-04-17 20:22:03 -0700

    ValentPaMixer: fixes and cleanups
    * fix typo removing outputs from the inputs hash table
    * handle pulseaudio server restarts
    * query default input/output before emitting when loading
    * ignore sink inputs and source outputs
    * set the adapter plugin priority to 100
    * drop alsa support

--- src/plugins/pulseaudio/valent-pa-mixer.c
+++ src/plugins/pulseaudio/valent-pa-mixer.c
@@ -6,6 +6,8 @@
 #include "config.h"
 
 #include <gvc-mixer-control.h>
+#include <gvc-mixer-sink.h>
+#include <gvc-mixer-source.h>
 #include <gvc-mixer-stream.h>
 #include <libvalent-core.h>
 #include <libvalent-mixer.h>
@@ -20,8 +22,7 @@ struct _ValentPaMixer
 
   GvcMixerControl    *control;
 
-  GHashTable         *inputs;
-  GHashTable         *outputs;
+  GHashTable         *streams;
   unsigned int        input;
   unsigned int        output;
   unsigned int        vol_max;
@@ -35,133 +36,177 @@ G_DEFINE_TYPE (ValentPaMixer, valent_pa_mixer, VALENT_TYPE_MIXER_ADAPTER)
  */
 static void
 on_default_sink_changed (GvcMixerControl *control,
-                         unsigned int     id,
+                         unsigned int     stream_id,
                          ValentPaMixer   *self)
 {
+  g_assert (GVC_IS_MIXER_CONTROL (control));
   g_assert (VALENT_IS_PA_MIXER (self));
 
-  if (self->output == id)
+  if (self->output == stream_id)
     return;
 
-  self->output = id;
+  self->output = stream_id;
   g_object_notify (G_OBJECT (self), "default-output");
 }
 
 static void
 on_default_source_changed (GvcMixerControl *control,
-                           unsigned int     id,
+                           unsigned int     stream_id,
                            ValentPaMixer   *self)
 {
+  g_assert (GVC_IS_MIXER_CONTROL (control));
   g_assert (VALENT_IS_PA_MIXER (self));
 
-  if (self->input == id)
+  if (self->input == stream_id)
     return;
 
-  self->input = id;
+  self->input = stream_id;
   g_object_notify (G_OBJECT (self), "default-input");
 }
 
 static void
-on_input_added (GvcMixerControl *control,
-                unsigned int     stream_id,
-                ValentPaMixer   *self)
-{
-  GvcMixerStream *gvc_stream;
-  ValentMixerStream *stream;
-
-  g_assert (VALENT_IS_PA_MIXER (self));
-
-  gvc_stream = gvc_mixer_control_lookup_stream_id (control, stream_id);
-
-  if G_UNLIKELY (gvc_stream == NULL)
-    return;
-
-  stream = g_object_new (VALENT_TYPE_PA_STREAM,
-                         "base-stream", gvc_stream,
-                         "direction",   VALENT_MIXER_INPUT,
-                         "vol-max",     self->vol_max,
-                         NULL);
-
-  g_hash_table_insert (self->inputs, GUINT_TO_POINTER (stream_id), stream);
-  valent_mixer_adapter_emit_stream_added (VALENT_MIXER_ADAPTER (self), stream);
-}
-
-static void
-on_input_removed (GvcMixerControl *control,
-                  unsigned int     stream_id,
-                  ValentPaMixer   *self)
-{
-  ValentMixerStream *stream;
-
-  g_assert (VALENT_IS_PA_MIXER (self));
-
-  stream = g_hash_table_lookup (self->inputs, GUINT_TO_POINTER (stream_id));
-
-  if G_UNLIKELY (stream == NULL)
-    return;
-
-  valent_mixer_adapter_emit_stream_removed (VALENT_MIXER_ADAPTER (self), stream);
-  g_hash_table_remove (self->inputs, GUINT_TO_POINTER (stream_id));
-}
-
-static void
-on_output_added (GvcMixerControl *control,
+on_stream_added (GvcMixerControl *control,
                  unsigned int     stream_id,
                  ValentPaMixer   *self)
 {
-  GvcMixerStream *gvc_stream;
-  ValentMixerStream *stream;
+  GvcMixerStream *base_stream = NULL;
+  ValentMixerStream *stream = NULL;
+  ValentMixerDirection direction;
 
-  g_assert (VALENT_IS_PA_MIXER (self));
+  base_stream = gvc_mixer_control_lookup_stream_id (control, stream_id);
 
-  gvc_stream = gvc_mixer_control_lookup_stream_id (control, stream_id);
-
-  if G_UNLIKELY (gvc_stream == NULL)
+  /* Ignore source outputs and sink inputs */
+  if (GVC_IS_MIXER_SINK (base_stream))
+    direction = VALENT_MIXER_OUTPUT;
+  else if (GVC_IS_MIXER_SOURCE (base_stream))
+    direction = VALENT_MIXER_INPUT;
+  else
     return;
 
   stream = g_object_new (VALENT_TYPE_PA_STREAM,
-                         "base-stream", gvc_stream,
-                         "direction",   VALENT_MIXER_OUTPUT,
+                         "base-stream", base_stream,
+                         "direction",   direction,
                          "vol-max",     self->vol_max,
                          NULL);
 
-  g_hash_table_insert (self->outputs, GUINT_TO_POINTER (stream_id), stream);
+  if (!g_hash_table_replace (self->streams, GUINT_TO_POINTER (stream_id), stream))
+    {
+      g_warning ("%s: Duplicate Stream: %s",
+                 G_OBJECT_TYPE_NAME (self),
+                 valent_mixer_stream_get_name (stream));
+    }
+
   valent_mixer_adapter_emit_stream_added (VALENT_MIXER_ADAPTER (self), stream);
 }
 
 static void
-on_output_removed (GvcMixerControl *control,
+on_stream_removed (GvcMixerControl *control,
                    unsigned int     stream_id,
                    ValentPaMixer   *self)
 {
-  ValentMixerStream *stream;
+  ValentMixerStream *stream = NULL;
 
+  g_assert (GVC_IS_MIXER_CONTROL (control));
   g_assert (VALENT_IS_PA_MIXER (self));
 
-  stream = g_hash_table_lookup (self->inputs, GUINT_TO_POINTER (stream_id));
+  stream = g_hash_table_lookup (self->streams, GUINT_TO_POINTER (stream_id));
 
-  if G_UNLIKELY (stream == NULL)
+  if (stream == NULL)
     return;
 
+  /* FIXME: If the stream being removed is the default, the change notification
+   *        will come after the removal notification. As a side effect, if the
+   *        kdeconnect-android activity is open it will automatically select a
+   *        remaining stream and override any automatic selection the system
+   *        wants to perform.
+   */
+
   valent_mixer_adapter_emit_stream_removed (VALENT_MIXER_ADAPTER (self), stream);
-  g_hash_table_remove (self->outputs, GUINT_TO_POINTER (stream_id));
+  g_hash_table_remove (self->streams, GUINT_TO_POINTER (stream_id));
 }
 
 static void
-on_stream_changed (GvcMixerControl *gvc_control,
+on_stream_changed (GvcMixerControl *control,
                    unsigned int     stream_id,
                    ValentPaMixer   *self)
 {
   ValentMixerAdapter *adapter = VALENT_MIXER_ADAPTER (self);
   ValentMixerStream *stream;
 
-  stream = g_hash_table_lookup (self->outputs, GUINT_TO_POINTER (stream_id));
-
-  if (stream == NULL)
-    stream = g_hash_table_lookup (self->inputs, GUINT_TO_POINTER (stream_id));
+  stream = g_hash_table_lookup (self->streams, GUINT_TO_POINTER (stream_id));
 
   if (stream != NULL)
     valent_mixer_adapter_emit_stream_changed (adapter, stream);
+}
+
+static void
+valent_pa_mixer_load (ValentPaMixer *self)
+{
+  g_autoptr (GSList) sources = NULL;
+  g_autoptr (GSList) sinks = NULL;
+  GvcMixerStream *stream = NULL;
+
+  g_assert (VALENT_IS_PA_MIXER (self));
+
+  /* Get current defaults */
+  self->vol_max = gvc_mixer_control_get_vol_max_norm (self->control);
+
+  /* Query the default streams before emitting signals */
+  if ((stream = gvc_mixer_control_get_default_sink (self->control)) != NULL)
+    self->output = gvc_mixer_stream_get_id (stream);
+
+  if ((stream = gvc_mixer_control_get_default_source (self->control)) != NULL)
+    self->input = gvc_mixer_stream_get_id (stream);
+
+  /* Get current streams */
+  sinks = gvc_mixer_control_get_sinks (self->control);
+
+  for (const GSList *iter = sinks; iter; iter = iter->next)
+    on_stream_added (self->control, gvc_mixer_stream_get_id (iter->data), self);
+
+  sources = gvc_mixer_control_get_sources (self->control);
+
+  for (const GSList *iter = sources; iter; iter = iter->next)
+    on_stream_added (self->control, gvc_mixer_stream_get_id (iter->data), self);
+
+  /* Watch stream changes */
+  g_object_connect (self->control,
+                    "signal::default-sink-changed",   on_default_sink_changed,   self,
+                    "signal::default-source-changed", on_default_source_changed, self,
+                    "signal::stream-added",           on_stream_added,           self,
+                    "signal::stream-removed",         on_stream_removed,         self,
+                    "signal::stream-changed",         on_stream_changed,         self,
+                    NULL);
+}
+
+static void
+valent_pa_mixer_unload (ValentPaMixer *self)
+{
+  ValentMixerAdapter *adapter = VALENT_MIXER_ADAPTER (self);
+  GHashTableIter iter;
+  ValentMixerStream *stream;
+
+  g_assert (VALENT_IS_PA_MIXER (self));
+
+  /* Clear the current defaults */
+  self->input = 0;
+  g_object_notify (G_OBJECT (self), "default-input");
+  self->output = 0;
+  g_object_notify (G_OBJECT (self), "default-output");
+
+  g_hash_table_iter_init (&iter, self->streams);
+
+  while (g_hash_table_iter_next (&iter, NULL, (void **)&stream))
+    {
+      valent_mixer_adapter_emit_stream_removed (adapter, stream);
+      g_hash_table_iter_remove (&iter);
+    }
+
+  g_signal_handlers_disconnect_by_func (self->control, on_default_sink_changed, self);
+  g_signal_handlers_disconnect_by_func (self->control, on_default_source_changed, self);
+  g_signal_handlers_disconnect_by_func (self->control, on_stream_added, self);
+  g_signal_handlers_disconnect_by_func (self->control, on_stream_removed, self);
+  g_signal_handlers_disconnect_by_func (self->control, on_stream_changed, self);
 }
 
 static void
@@ -169,53 +214,30 @@ on_state_changed (GvcMixerControl      *control,
                   GvcMixerControlState  state,
                   ValentPaMixer        *self)
 {
-  g_autoptr (GSList) sources = NULL;
-  g_autoptr (GSList) sinks = NULL;
-  GvcMixerStream *stream;
-
   g_assert (VALENT_IS_PA_MIXER (self));
 
-  if (state == GVC_STATE_FAILED)
+  switch (state)
     {
-      g_warning ("Failed to open PulseAudio mixer control");
-      return;
+    case GVC_STATE_CLOSED:
+      VALENT_NOTE ("%s: Closed", G_OBJECT_TYPE_NAME (self));
+      valent_pa_mixer_unload (self);
+      break;
+
+    case GVC_STATE_READY:
+      VALENT_NOTE ("%s: Ready", G_OBJECT_TYPE_NAME (self));
+      valent_pa_mixer_load (self);
+      break;
+
+    case GVC_STATE_CONNECTING:
+      VALENT_NOTE ("%s: Connecting", G_OBJECT_TYPE_NAME (self));
+      break;
+
+    case GVC_STATE_FAILED:
+      g_warning ("%s: Failed", G_OBJECT_TYPE_NAME (self));
+      valent_pa_mixer_unload (self);
+      break;
     }
-
-  if (state != GVC_STATE_READY)
-    return;
-
-  /* Get current streams */
-  sinks = gvc_mixer_control_get_sinks (self->control);
-
-  for (const GSList *iter = sinks; iter; iter = iter->next)
-    on_output_added (self->control, gvc_mixer_stream_get_id (iter->data), self);
-
-  sources = gvc_mixer_control_get_sources (self->control);
-
-  for (const GSList *iter = sources; iter; iter = iter->next)
-    on_input_added (self->control, gvc_mixer_stream_get_id (iter->data), self);
-
-  /* Get current defaults */
-  self->vol_max = gvc_mixer_control_get_vol_max_norm (self->control);
-
-  stream = gvc_mixer_control_get_default_sink (self->control);
-  self->output = gvc_mixer_stream_get_id (stream);
-
-  stream = gvc_mixer_control_get_default_source (self->control);
-  self->input = gvc_mixer_stream_get_id (stream);
-
-  /* Watch stream changes */
-  g_object_connect (self->control,
-                    "signal::default-sink-changed",   on_default_sink_changed,   self,
-                    "signal::default-source-changed", on_default_source_changed, self,
-                    "signal::input-added",            on_input_added,            self,
-                    "signal::input-removed",          on_input_removed,          self,
-                    "signal::output-added",           on_output_added,           self,
-                    "signal::output-removed",         on_output_removed,         self,
-                    "signal::stream-changed",         on_stream_changed,         self,
-                    NULL);
 }
-
 
 /*
  * ValentMixerAdapter
@@ -225,7 +247,7 @@ valent_pa_mixer_get_default_input (ValentMixerAdapter *adapter)
 {
   ValentPaMixer *self = VALENT_PA_MIXER (adapter);
 
-  return g_hash_table_lookup (self->inputs, GUINT_TO_POINTER (self->input));
+  return g_hash_table_lookup (self->streams, GUINT_TO_POINTER (self->input));
 }
 
 static void
@@ -245,7 +267,7 @@ valent_pa_mixer_get_default_output (ValentMixerAdapter *adapter)
 {
   ValentPaMixer *self = VALENT_PA_MIXER (adapter);
 
-  return g_hash_table_lookup (self->outputs, GUINT_TO_POINTER (self->output));
+  return g_hash_table_lookup (self->streams, GUINT_TO_POINTER (self->output));
 }
 
 static void
@@ -270,7 +292,6 @@ valent_pa_mixer_constructed (GObject *object)
 
   self->vol_max = gvc_mixer_control_get_vol_max_norm (self->control);
 
-  /* Open the base mixer control */
   g_signal_connect (self->control,
                     "state-changed",
                     G_CALLBACK (on_state_changed),
@@ -288,10 +309,7 @@ valent_pa_mixer_dispose (GObject *object)
   /* Close the mixer */
   g_signal_handlers_disconnect_by_data (self->control, self);
   gvc_mixer_control_close (self->control);
-
-  /* Drop sources and sinks */
-  g_hash_table_remove_all (self->inputs);
-  g_hash_table_remove_all (self->outputs);
+  g_hash_table_remove_all (self->streams);
 
   G_OBJECT_CLASS (valent_pa_mixer_parent_class)->dispose (object);
 }
@@ -301,8 +319,7 @@ valent_pa_mixer_finalize (GObject *object)
 {
   ValentPaMixer *self = VALENT_PA_MIXER (object);
 
-  g_clear_pointer (&self->inputs, g_hash_table_unref);
-  g_clear_pointer (&self->outputs, g_hash_table_unref);
+  g_clear_pointer (&self->streams, g_hash_table_unref);
   g_clear_object (&self->control);
 
   G_OBJECT_CLASS (valent_pa_mixer_parent_class)->finalize (object);
@@ -330,7 +347,6 @@ valent_pa_mixer_init (ValentPaMixer *self)
   self->control = g_object_new (GVC_TYPE_MIXER_CONTROL,
                                 "name", "Valent",
                                 NULL);
-  self->inputs = g_hash_table_new_full (NULL, NULL, NULL, g_object_unref);
-  self->outputs = g_hash_table_new_full (NULL, NULL, NULL, g_object_unref);
+  self->streams = g_hash_table_new_full (NULL, NULL, NULL, g_object_unref);
 }
 
