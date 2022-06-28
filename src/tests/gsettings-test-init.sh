1fe7f6f4a8424e684b83e500b99d4f5eed744992
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-06-28 15:42:16 -0700

    test(fix): fix ShellCheck errors in GSettings setup script
    Fix errors reported by ShellCheck and enable exit on error.

--- src/tests/gsettings-test-init.sh
+++ src/tests/gsettings-test-init.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: No rights reserved
 
 
 # Clean old files
-rm -rf ${GSETTINGS_SCHEMA_DIR}
-mkdir -p ${GSETTINGS_SCHEMA_DIR}
+rm -rf "${GSETTINGS_SCHEMA_DIR}"
+mkdir -p "${GSETTINGS_SCHEMA_DIR}"
 
 # Copy application and plugin GSettingsSchemas
-cp ${MESON_SOURCE_ROOT}/data/gsettings/*.gschema.xml ${GSETTINGS_SCHEMA_DIR}
-cp ${MESON_SOURCE_ROOT}/src/plugins/*/*.gschema.xml ${GSETTINGS_SCHEMA_DIR}
+cp "${MESON_SOURCE_ROOT}"/data/gsettings/*.gschema.xml "${GSETTINGS_SCHEMA_DIR}"
+cp "${MESON_SOURCE_ROOT}"/src/plugins/*/*.gschema.xml "${GSETTINGS_SCHEMA_DIR}"
 
