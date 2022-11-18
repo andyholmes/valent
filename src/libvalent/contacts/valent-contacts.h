2308d596e528301b20a35cc01b692f61a95db513
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-11-18 14:53:00 -0800

    refactor(contacts): remove `valent_contacts_lookup_store()`

--- src/libvalent/contacts/valent-contacts.h
+++ src/libvalent/contacts/valent-contacts.h
@@ -26,9 +26,6 @@ VALENT_AVAILABLE_IN_1_0
 ValentContactStore * valent_contacts_ensure_store (ValentContacts *contacts,
                                                    const char     *uid,
                                                    const char     *name);
-VALENT_AVAILABLE_IN_1_0
-ValentContactStore * valent_contacts_lookup_store (ValentContacts *contacts,
-                                                   const char     *uid);
 
 G_END_DECLS
 
