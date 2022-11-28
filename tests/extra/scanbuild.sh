bf1a950993d3fbd70a6c3a9e862b154c0473eb40
Andy Holmes <andrew.g.r.holmes@gmail.com>
2022-11-28 13:59:34 -0800

    ci: port CI to actuary

--- tests/extra/scanbuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-# SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: No rights reserved
-
-scan-build --status-bugs \
-           -disable-checker deadcode.DeadStores \
-           "$@"
