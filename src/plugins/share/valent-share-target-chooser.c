0af0a4c5f0cb9a00f542995a3a4a06dd9e7b6a96
Andy Holmes <andrew.g.r.holmes@gmail.com>
2023-02-19 20:34:04 -0800

    refactor(share): rework the share chooser dialog

--- src/plugins/share/valent-share-target-chooser.c
+++ src/plugins/share/valent-share-target-chooser.c
@@ -72,6 +72,7 @@ static GtkWidget *
 valent_share_target_chooser_create_row (gpointer item,
                                         gpointer user_data)
 {
+  ValentShareTargetChooser *self = VALENT_SHARE_TARGET_CHOOSER (user_data);
   ValentDevice *device = VALENT_DEVICE (item);
   GtkWidget *row;
 
@@ -80,12 +81,11 @@ valent_share_target_chooser_create_row (gpointer item,
   row = g_object_new (ADW_TYPE_ACTION_ROW,
                       "activatable",  TRUE,
                       "selectable",   FALSE,
-                      "margin-top",   12,
-                      "margin-start", 12,
-                      "margin-end",   12,
                       NULL);
-  gtk_widget_add_css_class (row, "card");
 
+  g_object_bind_property (self->device_list, "activate-on-single-click",
+                          row,               "selectable",
+                          G_BINDING_INVERT_BOOLEAN | G_BINDING_SYNC_CREATE);
   g_object_bind_property (device, "icon-name",
                           row,    "icon-name",
                           G_BINDING_DEFAULT | G_BINDING_SYNC_CREATE);
@@ -137,7 +137,7 @@ on_items_changed (GListModel               *list,
       g_autoptr (GtkWidget) widget = NULL;
 
       item = g_list_model_get_item (list, position + i);
-      widget = valent_share_target_chooser_create_row (item, NULL);
+      widget = valent_share_target_chooser_create_row (item, self);
 
       if (g_object_is_floating (widget))
         g_object_ref_sink (widget);
@@ -178,6 +178,29 @@ on_row_activated (GtkListBox               *box,
   gtk_window_close (GTK_WINDOW (self));
 }
 
+static void
+on_selected_rows_changed (GtkListBox               *box,
+                          ValentShareTargetChooser *self)
+{
+  g_autoptr (GList) rows = NULL;
+  unsigned int n_rows = 0;
+  unsigned int n_files = 0;
+
+  g_assert (GTK_IS_LIST_BOX (box));
+  g_assert (VALENT_IS_SHARE_TARGET_CHOOSER (self));
+
+  if ((rows = gtk_list_box_get_selected_rows (box)) != NULL)
+    n_rows = g_list_length (rows);
+
+  if (self->files != NULL)
+    n_files = g_list_model_get_n_items (self->files);
+
+  gtk_widget_action_set_enabled (GTK_WIDGET (self), "chooser.open",
+                                 (n_rows > 0 && n_files == 1));
+  gtk_widget_action_set_enabled (GTK_WIDGET (self), "chooser.share",
+                                 (n_rows > 0 && n_files >= 1));
+}
+
 static gboolean
 valent_share_target_chooser_refresh (gpointer data)
 {
@@ -188,6 +211,59 @@ valent_share_target_chooser_refresh (gpointer data)
   valent_device_manager_refresh (manager);
 
   return G_SOURCE_CONTINUE;
+}
+
+/*
+ * GAction
+ */
+static void
+chooser_cancel_action (GtkWidget  *widget,
+                       const char *action_name,
+                       GVariant   *parameter)
+{
+  ValentShareTargetChooser *self = VALENT_SHARE_TARGET_CHOOSER (widget);
+
+  g_assert (VALENT_IS_SHARE_TARGET_CHOOSER (self));
+
+  gtk_window_destroy (GTK_WINDOW (self));
+}
+
+static void
+chooser_open_action (GtkWidget  *widget,
+                     const char *action_name,
+                     GVariant   *parameter)
+{
+  ValentShareTargetChooser *self = VALENT_SHARE_TARGET_CHOOSER (widget);
+  GtkListBoxRow *row;
+  ValentDevice *device = NULL;
+  g_autoptr (GFile) file = NULL;
+  GVariant *target = NULL;
+
+  g_assert (VALENT_IS_SHARE_TARGET_CHOOSER (self));
+
+  if ((row = gtk_list_box_get_selected_row (self->device_list)) == NULL ||
+      g_list_model_get_n_items (self->files) == 0)
+    return;
+
+  device = g_object_get_data (G_OBJECT (row), "device");
+  file = g_list_model_get_item (self->files, 0);
+  target = g_variant_new_take_string (g_file_get_uri (file));
+
+  g_action_group_activate_action (G_ACTION_GROUP (device), "share.open", target);
+
+  gtk_window_close (GTK_WINDOW (self));
+}
+
+static void
+chooser_share_action (GtkWidget  *widget,
+                      const char *action_name,
+                      GVariant   *parameter)
+{
+  ValentShareTargetChooser *self = VALENT_SHARE_TARGET_CHOOSER (widget);
+  GtkListBoxRow *row;
+
+  row = gtk_list_box_get_selected_row (self->device_list);
+  on_row_activated (self->device_list, row, self);
 }
 
 /*
@@ -205,12 +281,12 @@ valent_share_target_chooser_constructed (GObject *object)
                            "items-changed",
                            G_CALLBACK (on_items_changed),
                            self, 0);
-
   on_items_changed (G_LIST_MODEL (self->manager),
                     0,
                     0,
                     g_list_model_get_n_items (G_LIST_MODEL (self->manager)),
                     self);
+  on_selected_rows_changed (self->device_list, self);
 
   /* Broadcast every 5 seconds to re-connect devices that may have gone idle */
   valent_device_manager_refresh (self->manager);
@@ -310,6 +386,11 @@ valent_share_target_chooser_class_init (ValentShareTargetChooserClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class, "/plugins/share/valent-share-target-chooser.ui");
   gtk_widget_class_bind_template_child (widget_class, ValentShareTargetChooser, device_list);
   gtk_widget_class_bind_template_callback (widget_class, on_row_activated);
+  gtk_widget_class_bind_template_callback (widget_class, on_selected_rows_changed);
+
+  gtk_widget_class_install_action (widget_class, "chooser.cancel", NULL, chooser_cancel_action);
+  gtk_widget_class_install_action (widget_class, "chooser.open", NULL, chooser_open_action);
+  gtk_widget_class_install_action (widget_class, "chooser.share", NULL, chooser_share_action);
 
   /**
    * ValentShareTargetChooser:device-manager:
