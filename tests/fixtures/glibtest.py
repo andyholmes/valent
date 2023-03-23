4d143eea26abd7efd93d8a4d0200a959f6c0ecec
Andy Holmes <andrew.g.r.holmes@gmail.com>
2023-03-22 17:08:18 -0700

    test: simplify `glibtest.py`

--- tests/fixtures/glibtest.py
+++ tests/fixtures/glibtest.py
@@ -11,111 +11,13 @@
 
 
 import os
-import re
 import subprocess
 import sys
-import functools
 import unittest
-from typing import Any, Callable, ClassVar, Optional, Type, TypeVar
+from typing import ClassVar
 
 
-GLibTestCaseAttr = TypeVar('GLibTestCaseAttr', bound='_GLibTestCaseAttr')
-
-class _GLibTestCaseAttr:
-    """An attribute which creates a bound method for running the test at the
-    specified path.
-    """
-
-    # pylint: disable=bad-staticmethod-argument,missing-function-docstring
-    # pylint: disable=too-few-public-methods
-
-    def __init__(self, path: str) -> None:
-        self.path = path
-
-    @staticmethod
-    def __func(self, path: str) -> None:
-        self._test_path(path)
-
-    # pylint: disable-next=line-too-long
-    def __get__(self, instance: Type[GLibTestCaseAttr], owner: Optional[Any]) -> Callable[[unittest.TestCase], None]:
-        # pylint: disable-next=no-value-for-parameter
-        bound_method = self.__func.__get__(instance, owner)
-        partial_method = functools.partial(bound_method, self.path)
-        partial_method.__doc__ = bound_method.__doc__
-        partial_method.__qualname__ = f'{owner.__qualname__}.{self.path}' # type: ignore
-
-        return partial_method
-
-
-GLibTestCaseMeta = TypeVar('GLibTestCaseMeta', bound='_GLibTestCaseMeta')
-
-class _GLibTestCaseMeta(type):
-    """Metaclass for :class:`~glibtest.GLibTestCase`."""
-
-    # pylint: disable-next=line-too-long,undefined-variable
-    def __new__(cls: Type[GLibTestCaseMeta], name: str, bases: tuple, namespace: dict) -> GLibTestCaseMeta:
-        test_case = type.__new__(cls, name, bases, namespace)
-
-        if test_case.executable: # type: ignore
-            _GLibTestCaseMeta.query_tests(test_case)
-
-        return test_case
-
-    @staticmethod
-    def query_tests(test_case: GLibTestCaseMeta) -> None:
-        """Load the test cases from :attr:`~glibtest.GLibTestCase.executable`.
-
-        A function attribute is set on ``test_case`` for each result so the
-        runner executes them in the proper order. In other words, each GLib
-        test case becomes a single test of a :class:`~unittest.TestCase`.
-        """
-
-        env = os.environ.copy()
-        env['G_MESSAGES_DEBUG'] = ''
-
-        try:
-            tests = subprocess.run([test_case.executable, '-l'], # type: ignore
-                                   capture_output=True,
-                                   check=True,
-                                   encoding='utf-8',
-                                   env=env,
-                                   timeout=15)
-        except subprocess.TimeoutExpired as error:
-            sys.stderr.write(error.stdout.decode('utf-8')) # type: ignore
-            raise RuntimeError(f'Querying tests: {error}') from error
-        except subprocess.CalledProcessError as error:
-            sys.stderr.write(error.stdout)
-            raise RuntimeError(f'Querying tests: {error}') from error
-        else:
-            lines = tests.stdout.split('\n')
-            n_tests = 0
-            version = int(re.findall(r'TAP version (\d+)|$', lines[0])[0])
-
-            for line in lines:
-                # Filter empty lines
-                if not line:
-                    continue
-
-                # pylint: disable-next=fixme
-                # TODO: validate test plan
-                if line.startswith('1..'):
-                    continue
-
-                # See: glib@f893df4b7d52cce6c2abcd485a8cfc15320835b7
-                if version == 13 and line.startswith('# /'):
-                    line = line[2:]
-
-                # Filter diagnostics
-                if line.startswith('#'):
-                    continue
-
-                n_tests += 1
-                setattr(test_case,
-                        f'test_{n_tests:03d}_{line}',
-                        _GLibTestCaseAttr(line))
-
-
-class GLibTestCase(metaclass = _GLibTestCaseMeta):
+class GLibTestCase:
     """A :class:`~unittest.TestCase` mixin for wrapping GLib-based test
     executables, especially with python-dbusmock.
 
@@ -128,35 +30,22 @@ class GLibTestCase(metaclass = _GLibTestCaseMeta):
 
     executable: ClassVar[str] = os.environ.get('G_TEST_EXE', '')
 
-    def _test_path(self, path: str) -> None:
-        """Run the test at ``path`` in the GLib-based executable at
-        :attr:`~glibtest.GLibTestCase.executable`.
+    def test_run(self) -> None:
+        """Run the executable at :attr:`~glibtest.GLibTestCase.executable` and
+        propagate the TAP output.
         """
 
         try:
-            test = subprocess.run([self.executable, '-p', path],
+            test = subprocess.run([self.executable, '--tap'],
                                   capture_output=True,
                                   check=True,
                                   encoding='utf-8')
 
-        # NOTE: If the meson timeout is reached, *this* process will be killed
-        #       and output from the test executable will not be propagated.
-        except subprocess.TimeoutExpired as error:
-            stdout = '' if not error.stdout else error.stdout.decode('utf-8')
-            stderr = '' if not error.stderr else error.stderr.decode('utf-8')
-
-            # pylint: disable-next=no-member
-            self.fail(f'\nstdout:\n{stdout}\nstderr:\n{stderr}') # type: ignore
-
-        # On failure, preserve the output and pipe
-        except subprocess.CalledProcessError as error:
-            # pylint: disable-next=no-member
-            self.fail(f'\nstdout:\n{error.stdout}\nstderr:\n{error.stderr}') # type: ignore
-
-        # On success, propagate the output verbatim
-        else:
             sys.stdout.write(test.stdout)
             sys.stderr.write(test.stderr)
+        except subprocess.SubprocessError as error:
+            # pylint: disable-next=no-member
+            self.fail(error.stdout) # type: ignore
 
 
 def main() -> None:
@@ -164,6 +53,6 @@ def main() -> None:
     ``__main__`` function instead of :func:`~unittest.main`.
     """
 
-    # avoid writing to stderr
-    runner = unittest.TextTestRunner(stream=sys.stdout, verbosity=2)
+    # Output to stderr; we're forwarding TAP output of the real program
+    runner = unittest.TextTestRunner(stream=sys.stderr, verbosity=2)
     unittest.main(testRunner=runner)
