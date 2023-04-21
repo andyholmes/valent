325e641e174c001cab474c4650a6733b63c23f69
Andy Holmes <andrew.g.r.holmes@gmail.com>
2023-04-20 17:58:38 -0700

    refactor(plugin): expand `ValentApplicationPlugin` vtable
    Add the following vfuncs:
    
    * `dbus_register()`
    * `dbus_unregister()`
    * `shutdown()`
    * `startup()`

--- src/libvalent/core/valent-application-plugin.c
+++ src/libvalent/core/valent-application-plugin.c
@@ -98,6 +98,30 @@ valent_application_plugin_real_command_line (ValentApplicationPlugin *plugin,
 }
 
 static gboolean
+valent_application_plugin_real_dbus_register (ValentApplicationPlugin  *plugin,
+                                              GDBusConnection          *connection,
+                                              const char               *object_path,
+                                              GError                  **error)
+{
+  g_assert (VALENT_IS_APPLICATION_PLUGIN (plugin));
+  g_assert (G_IS_DBUS_CONNECTION (connection));
+  g_assert (g_variant_is_object_path (object_path));
+  g_assert (error == NULL || *error == NULL);
+
+  return TRUE;
+}
+
+static void
+valent_application_plugin_real_dbus_unregister (ValentApplicationPlugin *plugin,
+                                                GDBusConnection         *connection,
+                                                const char              *object_path)
+{
+  g_assert (VALENT_IS_APPLICATION_PLUGIN (plugin));
+  g_assert (G_IS_DBUS_CONNECTION (connection));
+  g_assert (g_variant_is_object_path (object_path));
+}
+
+static gboolean
 valent_application_plugin_real_open (ValentApplicationPlugin  *plugin,
                                      GFile                   **files,
                                      int                       n_files,
@@ -109,6 +133,18 @@ valent_application_plugin_real_open (ValentApplicationPlugin  *plugin,
   g_assert (hint != NULL);
 
   return FALSE;
+}
+
+static void
+valent_application_plugin_real_shutdown (ValentApplicationPlugin *plugin)
+{
+  g_assert (VALENT_IS_APPLICATION_PLUGIN (plugin));
+}
+
+static void
+valent_application_plugin_real_startup (ValentApplicationPlugin *plugin)
+{
+  g_assert (VALENT_IS_APPLICATION_PLUGIN (plugin));
 }
 /* LCOV_EXCL_STOP */
 
@@ -176,7 +212,11 @@ valent_application_plugin_class_init (ValentApplicationPluginClass *klass)
   klass->enable = valent_application_plugin_real_enable;
   klass->activate = valent_application_plugin_real_activate;
   klass->command_line = valent_application_plugin_real_command_line;
+  klass->dbus_register = valent_application_plugin_real_dbus_register;
+  klass->dbus_unregister = valent_application_plugin_real_dbus_unregister;
   klass->open = valent_application_plugin_real_open;
+  klass->shutdown = valent_application_plugin_real_shutdown;
+  klass->startup = valent_application_plugin_real_startup;
 
   /**
    * ValentApplicationPlugin:application: (getter get_application):
@@ -343,6 +383,80 @@ valent_application_plugin_command_line (ValentApplicationPlugin *plugin,
 }
 
 /**
+ * valent_application_plugin_dbus_register: (virtual dbus_register)
+ * @plugin: a #ValentApplicationPlugin
+ * @connection: a `Gio.DBusCOnnection`
+ * @object_path: a D-Bus object path
+ * @error: (nullable): a `GError`
+ *
+ * Handle the D-Bus registration phase of the application.
+ *
+ * Implementations may override this method to export extra objects on the
+ * bus, that need to exist before the application tries to own the bus name.
+ *
+ * D-Bus registration will be aborted if %FALSE is returned, so implementations
+ * may return %TRUE and report the error by other means if it is not intended
+ * to be fatal.
+ *
+ * Returns: %TRUE, or %FALSE with @error set
+ *
+ * Since: 1.0
+ */
+gboolean
+valent_application_plugin_dbus_register (ValentApplicationPlugin  *plugin,
+                                         GDBusConnection          *connection,
+                                         const char               *object_path,
+                                         GError                  **error)
+{
+  gboolean ret;
+
+  VALENT_ENTRY;
+
+  g_return_val_if_fail (VALENT_IS_APPLICATION_PLUGIN (plugin), FALSE);
+  g_return_val_if_fail (G_IS_DBUS_CONNECTION (connection), FALSE);
+  g_return_val_if_fail (g_variant_is_object_path (object_path), FALSE);
+  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+
+  ret = VALENT_APPLICATION_PLUGIN_GET_CLASS (plugin)->dbus_register (plugin,
+                                                                     connection,
+                                                                     object_path,
+                                                                     error);
+
+  VALENT_RETURN (ret);
+}
+
+/**
+ * valent_application_plugin_dbus_unregister: (virtual dbus_unregister)
+ * @plugin: a #ValentApplicationPlugin
+ * @connection: a `Gio.DBusCOnnection`
+ * @object_path: a D
+ *
+ * Handle the D-Bus unregistration phase of the application.
+ *
+ * Implementations should override this method to unexport anything exported in
+ * [vfunc@Valent.ApplicationPlugin.dbus_register].
+ *
+ * Since: 1.0
+ */
+void
+valent_application_plugin_dbus_unregister (ValentApplicationPlugin  *plugin,
+                                           GDBusConnection          *connection,
+                                           const char               *object_path)
+{
+  VALENT_ENTRY;
+
+  g_return_if_fail (VALENT_IS_APPLICATION_PLUGIN (plugin));
+  g_return_if_fail (G_IS_DBUS_CONNECTION (connection));
+  g_return_if_fail (g_variant_is_object_path (object_path));
+
+  VALENT_APPLICATION_PLUGIN_GET_CLASS (plugin)->dbus_unregister (plugin,
+                                                                 connection,
+                                                                 object_path);
+
+  VALENT_EXIT;
+}
+
+/**
  * valent_application_plugin_open: (virtual open)
  * @plugin: a #ValentApplicationPlugin
  * @files: (array length=n_files): an array of #GFiles to open
@@ -380,5 +494,51 @@ valent_application_plugin_open (ValentApplicationPlugin  *plugin,
                                                             hint);
 
   VALENT_RETURN (ret);
+}
+
+/**
+ * valent_application_plugin_shutdown: (virtual shutdown)
+ * @plugin: a #ValentApplicationPlugin
+ *
+ * Handle the shutdown phase of the application.
+ *
+ * Implementations should override this method to reverse anything done in
+ * [vfunc@Valent.ApplicationPlugin.startup].
+ *
+ * Since: 1.0
+ */
+void
+valent_application_plugin_shutdown (ValentApplicationPlugin *plugin)
+{
+  VALENT_ENTRY;
+
+  g_return_if_fail (VALENT_IS_APPLICATION_PLUGIN (plugin));
+
+  VALENT_APPLICATION_PLUGIN_GET_CLASS (plugin)->shutdown (plugin);
+
+  VALENT_EXIT;
+}
+
+/**
+ * valent_application_plugin_startup: (virtual startup)
+ * @plugin: a #ValentApplicationPlugin
+ *
+ * Handle the startup phase of the application.
+ *
+ * Implementations may override this method to perform setup task that should
+ * only happen on the primary instance.
+ *
+ * Since: 1.0
+ */
+void
+valent_application_plugin_startup (ValentApplicationPlugin *plugin)
+{
+  VALENT_ENTRY;
+
+  g_return_if_fail (VALENT_IS_APPLICATION_PLUGIN (plugin));
+
+  VALENT_APPLICATION_PLUGIN_GET_CLASS (plugin)->startup (plugin);
+
+  VALENT_EXIT;
 }
 
