d818f2096a6fe6d4f81bab16318344f3dd8303d0
Andy Holmes <andrew.g.r.holmes@gmail.com>
2024-08-05 19:34:59 -0700

    feat(messages): add support for thread participants

--- src/libvalent/messages/valent-messages-adapter-private.h
+++ src/libvalent/messages/valent-messages-adapter-private.h
@@ -9,23 +9,26 @@
 
 G_BEGIN_DECLS
 
-/*< private>
- *
- * Cursor columns for `vmo:PhoneMessage`.
+/*
+ * SPARQL Cursor Columns
  */
-#define CURSOR_MESSAGE_IRI                0
-#define CURSOR_MESSAGE_BOX                1
-#define CURSOR_MESSAGE_DATE               2
-#define CURSOR_MESSAGE_ID                 3
-#define CURSOR_MESSAGE_READ               4
-#define CURSOR_MESSAGE_RECIPIENTS         5
-#define CURSOR_MESSAGE_SENDER             6
-#define CURSOR_MESSAGE_SUBSCRIPTION_ID    7
-#define CURSOR_MESSAGE_TEXT               8
-#define CURSOR_MESSAGE_THREAD_ID          9
-#define CURSOR_MESSAGE_ATTACHMENT_IRI     10
-#define CURSOR_MESSAGE_ATTACHMENT_PREVIEW 11
-#define CURSOR_MESSAGE_ATTACHMENT_FILE    12
+#define CURSOR_MESSAGE_IRI                 (0)
+#define CURSOR_MESSAGE_BOX                 (1)
+#define CURSOR_MESSAGE_DATE                (2)
+#define CURSOR_MESSAGE_ID                  (3)
+#define CURSOR_MESSAGE_READ                (4)
+#define CURSOR_MESSAGE_RECIPIENTS          (5)
+#define CURSOR_MESSAGE_SENDER              (6)
+#define CURSOR_MESSAGE_SUBSCRIPTION_ID     (7)
+#define CURSOR_MESSAGE_TEXT                (8)
+#define CURSOR_MESSAGE_THREAD_ID           (9)
+
+#define CURSOR_MESSAGE_ATTACHMENT_IRI      (10)
+#define CURSOR_MESSAGE_ATTACHMENT_PREVIEW  (11)
+#define CURSOR_MESSAGE_ATTACHMENT_FILE     (12)
+
+#define CURSOR_MESSAGE_THREAD_IRI          (10)
+#define CURSOR_MESSAGE_THREAD_PARTICIPANTS (11)
 
 
 ValentMessage * valent_message_from_sparql_cursor (TrackerSparqlCursor *cursor,
