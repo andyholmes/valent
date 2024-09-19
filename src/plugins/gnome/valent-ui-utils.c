d08b14874622803ab764e7b7cd633f8edee216a2
Andy Holmes <andrew.g.r.holmes@gmail.com>
2024-09-18 17:17:42 -0700

    refactor(gnome): cleanup URI highlighter

--- src/plugins/gnome/valent-ui-utils.c
+++ src/plugins/gnome/valent-ui-utils.c
@@ -5,20 +5,16 @@
 
 #include "config.h"
 
-#include <gtk/gtk.h>
-#include <pango/pango.h>
-#include <valent.h>
+#include <gio/gio.h>
 
 #include "valent-ui-utils-private.h"
 
 
-// Andy Holmes
-#define EMAIL_PATTERN "\\b(?:.+@.+\\.[a-z]{2,4}\\b)"
-
-// https://gist.github.com/gruber/8891611 (changed tld list to `[a-z]{2,4}`)
-#define URL_PATTERN "\\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\\-]+[.](?:[a-z]{2,4})/)(?:[^\\s()<>{}\\[\\]]+|\\([^\\s()]*?\\([^\\s()]+\\)[^\\s()]*?\\)|\\([^\\s]+?\\))+(?:\\([^\\s()]*?\\([^\\s()]+\\)[^\\s()]*?\\)|\\([^\\s]+?\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\\-][a-z0-9]+)*[.](?:[a-z]{2,4})\\b/?(?!@)))"
-
-#define URI_FLAGS   (G_REGEX_CASELESS | G_REGEX_MULTILINE | G_REGEX_NO_AUTO_CAPTURE | G_REGEX_OPTIMIZE)
+// https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+#define EMAIL_PATTERN "[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*"
+// https://mathiasbynens.be/demo/url-regex, @stephenhay, relaxed scheme
+#define URI_PATTERN   "\\b([a-zA-Z0-9-]+:[\\/]{1,3}|www[.])[^\\s>]*"
+#define COMPILE_FLAGS (G_REGEX_CASELESS | G_REGEX_MULTILINE | G_REGEX_NO_AUTO_CAPTURE | G_REGEX_OPTIMIZE)
 
 static GRegex *email_regex = NULL;
 static GRegex *uri_regex = NULL;
@@ -28,28 +24,16 @@ valent_ui_replace_eval_uri (const GMatchInfo *info,
                             GString          *result,
                             gpointer          user_data)
 {
-  g_autofree char *text = NULL;
-  g_autofree char *escaped = NULL;
+  g_autofree char *uri = NULL;
 
-  text = g_match_info_fetch (info, 0);
+  uri = g_match_info_fetch (info, 0);
 
-  if (g_uri_is_valid (text, G_URI_FLAGS_NONE, NULL))
-    {
-      escaped = g_markup_printf_escaped ("<a href=\"%s\">%s</a>",
-                                         text, text);
-    }
-  else if (g_regex_match (email_regex, text, 0, NULL))
-    {
-      escaped = g_markup_printf_escaped ("<a href=\"mailto:%s\">%s</a>",
-                                         text, text);
-    }
+  if (g_uri_is_valid (uri, G_URI_FLAGS_NONE, NULL))
+    g_string_append_printf (result, "<a href=\"%s\">%s</a>", uri, uri);
+  else if (g_regex_match (email_regex, uri, 0, NULL))
+    g_string_append_printf (result, "<a href=\"mailto:%s\">%s</a>", uri, uri);
   else
-    {
-      escaped = g_markup_printf_escaped ("<a href=\"https://%s\">%s</a>",
-                                         text, text);
-    }
-
-  g_string_append (result, escaped);
+    g_string_append_printf (result, "<a href=\"https://%s\">%s</a>", uri, uri);
 
   return FALSE;
 }
@@ -65,15 +49,15 @@ valent_ui_replace_eval_uri (const GMatchInfo *info,
  *
  * If @text is %NULL, this function will return %NULL.
  *
- * Returns: (transfer full) (nullable): a string of Pango markup
+ * Returns: (transfer full) (nullable): a string of markup
  *
  * Since: 1.0
  */
 char *
 valent_string_to_markup (const char *text)
 {
-  g_autofree char *result = NULL;
-  int text_len = 0;
+  g_autofree char *escaped = NULL;
+  g_autofree char *markup = NULL;
   g_autoptr (GError) error = NULL;
 
   if G_UNLIKELY (text == NULL)
@@ -81,32 +65,19 @@ valent_string_to_markup (const char *text)
 
   if G_UNLIKELY (uri_regex == NULL)
     {
-      email_regex = g_regex_new (EMAIL_PATTERN, URI_FLAGS, 0, NULL);
-      uri_regex = g_regex_new (URL_PATTERN"|"EMAIL_PATTERN, URI_FLAGS, 0, NULL);
+      email_regex = g_regex_new (EMAIL_PATTERN, COMPILE_FLAGS, 0, NULL);
+      uri_regex = g_regex_new (URI_PATTERN"|"EMAIL_PATTERN, COMPILE_FLAGS, 0, NULL);
     }
 
-  text_len = strlen (text);
-  result = g_regex_replace_eval (uri_regex,
-                                 text,
-                                 text_len,
+  escaped = g_markup_escape_text (text, -1);
+  markup = g_regex_replace_eval (uri_regex,
+                                 escaped,
+                                 strlen (escaped),
                                  0,
                                  0,
                                  valent_ui_replace_eval_uri,
                                  NULL,
                                  &error);
 
-  if (result == NULL)
-    {
-      g_warning ("%s: %s: %s", G_STRFUNC, error->message, text);
-      return g_markup_escape_text (text, text_len);
-    }
-
-  if (!pango_parse_markup (result, -1, 0, NULL, NULL, NULL, &error) &&
-      !g_error_matches (error, G_MARKUP_ERROR, G_MARKUP_ERROR_UNKNOWN_ELEMENT))
-    {
-      g_warning ("%s: %s: %s", G_STRFUNC, error->message, result);
-      return g_markup_escape_text (text, text_len);
-    }
-
-  return g_steal_pointer (&result);
+  return g_steal_pointer (&markup);
 }
