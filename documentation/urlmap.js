d1170668e1078333ec7408579faea07b2458f607
andyholmes <1265208+andyholmes@users.noreply.github.com>
2025-03-02 23:41:26 +0000

    Deploying to gh-pages from @ andyholmes/valent@f15de40bdad1e86aa9b46ff60620191219447f5b 🚀

---
+++ documentation/urlmap.js
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2021 GNOME Foundation
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
+    [ 'Gsk', 'https://docs.gtk.org/gsk4/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+    [ 'Json', 'https://gnome.pages.gitlab.gnome.org/json-glib/'],
+    [ 'Peas', 'https://gnome.pages.gitlab.gnome.org/libpeas/libpeas-1.0/'],
+    [ 'Adw', 'https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/' ],
+]
+
