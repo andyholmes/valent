2bd04cb96caa04311decfd5f87812ef67fab95e3
Andy Holmes <andrew.g.r.holmes@gmail.com>
2025-11-07 13:04:27 -0800

    build: coalesce core GSchemas into a single file

--- tests/extra/gsettings-test-init.sh
+++ tests/extra/gsettings-test-init.sh
@@ -9,6 +9,6 @@ rm -rf "${GSETTINGS_SCHEMA_DIR}"
 mkdir -p "${GSETTINGS_SCHEMA_DIR}"
 
 # Copy application and plugin GSettingsSchemas
-cp "${MESON_SOURCE_ROOT}"/data/gsettings/*.gschema.xml "${GSETTINGS_SCHEMA_DIR}"
+cp "${MESON_SOURCE_ROOT}"/data/*.gschema.xml "${GSETTINGS_SCHEMA_DIR}"
 cp "${MESON_SOURCE_ROOT}"/src/plugins/*/*.gschema.xml "${GSETTINGS_SCHEMA_DIR}"
 
